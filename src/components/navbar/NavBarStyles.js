bdd7de234dc4798eca411a7a0e0c0b090cf9f9ad
Banjer71 <davgio7@gmail.com>
2020-03-13 22:49:43 +0000

    change the name of the menu button and crerate the mobile menu

--- src/components/navbar/NavBarStyles.js
+++ src/components/navbar/NavBarStyles.js
@@ -1,15 +1,79 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+export const BtnContet = styled.div`
+	border: 1px solid green;
+	display: inline-block;
+
+	> .bar {
+		width: 30px;
+		margin: 3px;
+		height: 4px;
+		color: rgb(139, 139, 137);
+		background: grey;
+	}
+
+    @media screen and (min-width: 780px) {
+		display: none;
+	}
+`;
+
+export const Nav = styled.div`
+max-width: 1200px;
+margin: 0 auto;
+
+@media screen and (min-width: 780px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+`;
+
+export const LogoAndBtn = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+cursor: pointer;
+`;
+
+export const Logo = styled.div`
+cursor: pointer;
+padding: 15px;
+
+> img {
+    width: 90px;
+}
+`;
+
+export const Navigation = styled.ul`
+height: 0;
+transition: all 0.5s ease;
+overflow: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
 
 
-export const Btn = styled.div`
-border: 1px solid green;
-display: inline-block;
+> li a {
+    background-color: rgb(234, 234, 234);
+    display: block;
+    padding-bottom: 20px;
+    
+    cursor: pointer;
+}
 
-> .bar {
-    width: 30px;
-    margin: 3px;
-    height: 4px;
-    color: rgb(139,139,137);
-    background: grey;
+> li a:hover {
+    background-color: #efece1;
+}
+
+@media screen and (min-width: 780px) {
+    overflow: ${(props) => (props.isOpen ? 'visible' : 'visible')};
+    display: flex;
+
+    > li a {
+        background-color: transparent;
+        padding: 15px;
+        transition: all 0.5s ease;
+    }
+
+    > li a:hover {
+        background-color: #efece1;
+    }
 }
 `;

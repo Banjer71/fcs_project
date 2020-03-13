cb90504b1a45b836d3f08916832deec329a40e7f
Banjer71 <davgio7@gmail.com>
2020-03-13 13:46:11 +0000

    create a js file where to store all the styled component

--- src/components/navbar/togglemenu/ToggleMenu.js
+++ src/components/navbar/togglemenu/ToggleMenu.js
@@ -1,18 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
-
-const Btn = styled.div`
-    border: 1px solid green;
-    display: inline-block;
-
-    > .bar {
-        width: 30px;
-        margin: 3px;
-        height: 4px;
-        color: rgb(139,139,137);
-        background: grey;
-    }
-`;
+import {Btn} from '../NavBarStyles'
 
 
 const ToggleMenu = () => {

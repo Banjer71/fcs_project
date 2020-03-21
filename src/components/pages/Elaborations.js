e63dd83766d19782162b1221afa17e44490fa467
Davide Naccarati <davgio7@gmail.com>
2020-03-21 09:34:05 +0000

    Revert "Revert "uploaded images for the sliders component" (#10)"
    This reverts commit 122574fc1665116fee22fb7e7b031cebe152777a.

--- src/components/pages/Elaborations.js
+++ src/components/pages/Elaborations.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container } from './PagesStyles';
 
 const Content = styled.div`
 	width: 100%;
-	outline: 1px dashed red;
 `;
 
 const Elaborations = () => {
-	return (
-		<Content>
-			<Container>
-				
-			</Container>
-			<Container />
-		</Content>
-	);
+	return <Content />;
 };
 
 export default Elaborations;

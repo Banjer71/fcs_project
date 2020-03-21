99cd10a12c4e556867db07e6e65b78ff0872664e
unknown <davgio7@gmail.com>
2020-03-21 19:46:33 +0000

    added new flip card motion to the card component

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -2,13 +2,60 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ColCard = styled.div`
-	margin: 0 auto;
+	width: 300px;
+	height: 430px;
+	margin: 0 auto 120px auto;
 	grid-column: ${(props) => (props.fromCollab ? '1/9' : '1')};
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-start;
 	align-items: left;
+	perspective: 120rem;
+	
 
+	&:hover .flip-card  {
+		transform: rotateY(180deg);
+
+	}
+
+	.flip-card {
+		width: 100%;
+		height: 100%;
+		position: relative;
+		transition: transform 1s;
+		transform-style: preserve-3d;
+	}
+
+	.flip-card-front, .flip-card-back {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		backface-visibility: hidden;
+	}
+
+	.flip-card-back {
+		background-color: #517fa4;
+		transform: rotateY(180deg);
+		color: #eaeaea;
+
+		> h3 {
+			padding: 3px;
+		}
+
+		> h3:first-child {
+			padding-top: 30px;
+		}
+
+		> p, h4 {
+			margin-top: 15px;
+			padding: 0 20px;
+		}
+
+		> h4 {
+			padding-top: 20px;
+		}
+	}
+	
 	.col-card-info {
 		padding-top: 25px;
 		padding-left: 10px;
@@ -41,13 +88,24 @@ const ColCard = styled.div`
 const Card = (props) => {
 	return (
 		<ColCard fromCollab={props.fromCollab}>
-			<div className="col-card-img">
-				<img src={props.image} alt="poster movie" />
+			<div className='flip-card'>
+				<div className='flip-card-front col-card-img'>
+					<img src={props.image} alt="poster movie" />
+					<div className="col-card-info">
+						<h3>{props.name}</h3>
+						<p>{props.company}</p>
+					</div>
+				</div>
+				<div className="flip-card-back ">
+					<h3>{props.textTitle}</h3>
+					<h3>{props.textDesign}</h3>
+					<h3>{props.textName}</h3>
+					<h4>{props.objective}</h4>
+					<p>{props.text}</p>
+					<p>{props.text2}</p>
+				</div>
 			</div>
-			<div className="col-card-info">
-				<h3>{props.name}</h3>
-				<p>{props.company}</p>
-			</div>
+
 		</ColCard>
 	);
 };
--- src/components/pages/Elaborations.js
+++ src/components/pages/Elaborations.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import Catch22 from '../images/catch22.jpg';
 
 const Content = styled.div`
 	width: 100%;
 `;
 
+const Card = styled.div`
+
+`;
+
+
 const Elaborations = () => {
-	return <Content />;
+	return (
+		<Content>
+		
+		</Content>
+	)
 };
 
 export default Elaborations;
